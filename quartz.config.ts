e2694880bb8192d5c26b448b718f87cab9b4e272
kuro <jwjang@me.com>
2026-02-16 06:12:12 +0900

    chore: 위키 이름 변경 → Snail Wiki

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "IndieLoca Strategy",
+    pageTitle: "Snail Wiki",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
